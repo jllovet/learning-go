661fa10b46cea22d7f0a55557935315c3898b2a4
Jonathan Llovet <jonathan.llovet@gmail.com>
2020-07-09 00:44:58 -0400

    Add example of initialization and reassignment

--- 02-udemy-complete-dev-guide/02-deeper-into-go/main.go
+++ 02-udemy-complete-dev-guide/02-deeper-into-go/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var card string = "Ace of Spades"
+	// go is statically typed!
+	card := "Ace of Spades"  // Initialization
+	card = "Queen of Hearts" // Reassignment (note the difference in operator)
+	fmt.Println(card)
 }
