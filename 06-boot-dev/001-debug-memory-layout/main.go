6e9d47590a5d9d0407c62d41668f9325b31b89d4
Jonathan Llovet <jllovet@openingquestions.com>
2025-07-19 17:14:30 -0400

    Add memory debug exercise from boot.dev

---
+++ 06-boot-dev/001-debug-memory-layout/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type contact struct {
+	sendingLimit int32
+	age          int32
+	userID       string
+}
+
+type perms struct {
+	permissionLevel int
+	canSend         bool
+	canReceive      bool
+	canManage       bool
+}
+
+func main() {
+
+	typ := reflect.TypeOf(1)
+	fmt.Printf("Struct is %d bytes\n", typ.Size())
+
+	typ = reflect.TypeOf("")
+	fmt.Printf("Struct is %d bytes\n", typ.Size())
+
+	typ = reflect.TypeOf(contact{})
+	fmt.Printf("Struct is %d bytes\n", typ.Size())
+
+	typ = reflect.TypeOf(perms{})
+	fmt.Printf("Struct is %d bytes\n", typ.Size())
+}
