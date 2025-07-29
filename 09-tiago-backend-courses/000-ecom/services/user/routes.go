ab767f805b13e09c61a7f58c0241f40dabbbfdab
Jonathan Llovet <jllovet@openingquestions.com>
2025-07-29 14:52:09 -0400

    Add user repository with types and beginnings of auth package

--- 09-tiago-backend-courses/000-ecom/services/user/routes.go
+++ 09-tiago-backend-courses/000-ecom/services/user/routes.go
@@ -1,15 +1,21 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	ecomio "github.com/jllovet/learning-go/09-tiago-backend-courses/000-ecom/io"
+	"github.com/jllovet/learning-go/09-tiago-backend-courses/000-ecom/services/auth"
+	"github.com/jllovet/learning-go/09-tiago-backend-courses/000-ecom/types"
 )
 
-type Handler struct{}
+type Handler struct {
+	store types.UserStore
+}
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -20,6 +26,38 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 
 }
-func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := ecomio.ParseJSON(r, payload); err != nil {
+		ecomio.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	// check if the user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+
+	// if err -> user already exists
+	if err == nil {
+		ecomio.WriteError(w, http.StatusBadRequest, fmt.Errorf("user already exists: %s", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		ecomio.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	// if the user does not exist, then create the new user
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		ecomio.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	ecomio.WriteJSON(w, http.StatusCreated, nil)
 }
