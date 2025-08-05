eb329ff189d808af98dcd8c898aa2aa7dd8afdba
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-04 23:16:46 -0400

    Parameterize name in Hello

--- 10-learn-go-with-tests/00-go-fundamentals/01-hello-world/hello_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/01-hello-world/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello World"
+	got := Hello("Carl")
+	want := "Hello, Carl"
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
