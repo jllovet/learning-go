228424bf9bde9b1ce153bcb900bd5c99de74a686
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-05 00:02:24 -0400

    Refactor tests for hello world

--- 10-learn-go-with-tests/00-go-fundamentals/01-hello-world/hello_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/01-hello-world/hello_test.go
@@ -2,19 +2,22 @@ package main
 
 import "testing"
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestHello(t *testing.T) {
 	t.Run("say hello to person", func(t *testing.T) {
 		got := Hello("Carl")
 		want := "Hello, Carl"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
