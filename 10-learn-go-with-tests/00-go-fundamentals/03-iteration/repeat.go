9ecce7207050f715067cd6bd16ff3c793c03b009
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-17 13:07:10 -0400

    Refactor to use range

--- 10-learn-go-with-tests/00-go-fundamentals/03-iteration/repeat.go
+++ 10-learn-go-with-tests/00-go-fundamentals/03-iteration/repeat.go
@@ -3,7 +3,7 @@ package iteration
 func Repeat(s string, count int) string {
 	// see strings.Repeat
 	var repeated string
-	for i := 0; i < count; i++ {
+	for range count {
 		repeated += s
 	}
 	return repeated
