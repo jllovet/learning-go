4b35f7e2573b0e4fc21f458bc7c8f686fa92b723
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-16 21:22:49 -0400

    Add support for Spanish with English as default

--- 10-learn-go-with-tests/00-go-fundamentals/01-hello-world/main.go
+++ 10-learn-go-with-tests/00-go-fundamentals/01-hello-world/main.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const enGreetingPrefix = "Hello, "
+const esGreetingPrefix = "Hola, "
 
-func Hello(name string) string {
-	switch name {
-	case "":
-		return enGreetingPrefix + "World"
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	switch language {
+	case spanish:
+		return esGreetingPrefix + name
 	default:
 		return enGreetingPrefix + name
 	}
 }
 
 func main() {
-	fmt.Println(Hello("Jonathan"))
+	fmt.Println(Hello("Jonathan", ""))
 }
