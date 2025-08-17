848f41176c0f47bc8b1acf98054910097e24de55
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-17 18:40:57 -0400

    Add another version of Repeat for benchmark comparisons

--- 10-learn-go-with-tests/00-go-fundamentals/03-iteration/repeat.go
+++ 10-learn-go-with-tests/00-go-fundamentals/03-iteration/repeat.go
@@ -11,6 +11,14 @@ func Repeat(s string, count int) string {
 	return repeated
 }
 
+func BuilderRepeat(s string, count int) string {
+	var repeated strings.Builder
+	for range count {
+		repeated.WriteString(s)
+	}
+	return repeated.String()
+}
+
 func StdLibRepeat(s string, count int) string {
 	return strings.Repeat(s, count)
 }
--- 10-learn-go-with-tests/00-go-fundamentals/03-iteration/repeat_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/03-iteration/repeat_test.go
@@ -24,16 +24,105 @@ func TestRepeat(t *testing.T) {
 			t.Errorf("want %s but got %s", want, got)
 		}
 	})
+	t.Run("repeating long string", func(t *testing.T) {
+		got := Repeat("apple", 4)
+		want := "appleappleappleapple"
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
 }
 
-func BenchmarkRepeat(b *testing.B) {
+func TestBuilderRepeat(t *testing.T) {
+	t.Run("repeat string 4 times", func(t *testing.T) {
+		got := BuilderRepeat("a", 4)
+		want := "aaaa"
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+	t.Run("repeating empty string returns empty string", func(t *testing.T) {
+		got := BuilderRepeat("", 4)
+		want := ""
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+	t.Run("repeating string 5 times", func(t *testing.T) {
+		got := BuilderRepeat("a", 5)
+		want := "aaaaa"
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+	t.Run("repeating long string", func(t *testing.T) {
+		got := BuilderRepeat("apple", 4)
+		want := "appleappleappleapple"
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+}
+
+func TestStdLibRepeat(t *testing.T) {
+	t.Run("repeat string 4 times", func(t *testing.T) {
+		got := StdLibRepeat("a", 4)
+		want := "aaaa"
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+	t.Run("repeating empty string returns empty string", func(t *testing.T) {
+		got := StdLibRepeat("", 4)
+		want := ""
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+	t.Run("repeating string 5 times", func(t *testing.T) {
+		got := StdLibRepeat("a", 5)
+		want := "aaaaa"
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+	t.Run("repeating long string", func(t *testing.T) {
+		got := StdLibRepeat("apple", 4)
+		want := "appleappleappleapple"
+		if got != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+}
+
+func BenchmarkRepeatShortString(b *testing.B) {
 	for b.Loop() {
 		Repeat("a", 4)
 	}
 }
+func BenchmarkRepeatLongString(b *testing.B) {
+	for b.Loop() {
+		Repeat("apple", 4)
+	}
+}
+func BenchmarkBuilderRepeatShortString(b *testing.B) {
+	for b.Loop() {
+		BuilderRepeat("a", 4)
+	}
+}
+func BenchmarkBuilderRepeatLongString(b *testing.B) {
+	for b.Loop() {
+		BuilderRepeat("apple", 4)
+	}
+}
 
-func BenchmarkStdLibRepeat(b *testing.B) {
+func BenchmarkStdLibRepeatShortString(b *testing.B) {
 	for b.Loop() {
 		StdLibRepeat("a", 4)
+	}
+}
+func BenchmarkStdLibRepeatLongString(b *testing.B) {
+	for b.Loop() {
+		StdLibRepeat("apple", 4)
 	}
 }
