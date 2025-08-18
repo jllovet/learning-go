12d19d8a411f879a6dccbab75ffefe8187445b2e
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-18 16:13:02 -0400

    Switch in helper method for asserting slice equality in tests

--- 10-learn-go-with-tests/00-go-fundamentals/04-arrays-and-slices/sum_all_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/04-arrays-and-slices/sum_all_test.go
@@ -8,22 +8,16 @@ func TestSumAll(t *testing.T) {
 	t.Run("sum of one slice equals just the sum of that slice's elements", func(t *testing.T) {
 		got := SumAll([]int{1, 2, 3, 4, 5})
 		want := []int{15}
-		if !slices.Equal(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSlicesEqual(t, got, want)
 	})
 	t.Run("sum of 0 slices is empty slice", func(t *testing.T) {
 		got := SumAll()
 		want := []int{}
-		if !slices.Equal(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSlicesEqual(t, got, want)
 	})
 	t.Run("sum of multiple slices is the sum of the sums of their elements", func(t *testing.T) {
 		got := SumAll([]int{1, 2, 3, 4, 5}, []int{1, 2})
 		want := []int{15, 3}
-		if !slices.Equal(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSlicesEqual(t, got, want)
 	})
 }
