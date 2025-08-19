8592896345b4917a7f5eccf9b1d940aa4f32099b
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-19 18:56:48 -0400

    Add Area and rename files to geometry

--- 10-learn-go-with-tests/00-go-fundamentals/05-smi/geometry.go
+++ 10-learn-go-with-tests/00-go-fundamentals/05-smi/geometry.go
@@ -3,3 +3,7 @@ package smi
 func Perimeter(length float64, width float64) float64 {
 	return 2 * (length + width)
 }
+
+func Area(length float64, width float64) float64 {
+	return length * width
+}
--- 10-learn-go-with-tests/00-go-fundamentals/05-smi/geometry_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/05-smi/geometry_test.go
@@ -7,3 +7,9 @@ func TestPerimeter(t *testing.T) {
 	want := 40.0
 	assertCorrectMessage(t, got, want)
 }
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	want := 100.0
+	assertCorrectMessage(t, got, want)
+}
