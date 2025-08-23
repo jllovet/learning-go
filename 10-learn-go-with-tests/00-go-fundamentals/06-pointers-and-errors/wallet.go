c32d63b6da1b79e3ea98929b3c32f00c9dc5e194
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-23 19:21:26 -0400

    Add custom type to wrap int in wallets

--- 10-learn-go-with-tests/00-go-fundamentals/06-pointers-and-errors/wallet.go
+++ 10-learn-go-with-tests/00-go-fundamentals/06-pointers-and-errors/wallet.go
@@ -1,13 +1,14 @@
 package pointersanderrors
 
+type Bitcoin int
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
