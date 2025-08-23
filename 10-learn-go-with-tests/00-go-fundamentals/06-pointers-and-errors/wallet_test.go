c219e87fb0dc25cf3616fb923ff0ab0613db0f67
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-23 19:57:22 -0400

    Add handling of insufficient funds on withdrawal

--- 10-learn-go-with-tests/00-go-fundamentals/06-pointers-and-errors/wallet_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/06-pointers-and-errors/wallet_test.go
@@ -3,7 +3,6 @@ package pointersanderrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-
 	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Helper()
 		got := wallet.Balance()
@@ -11,7 +10,14 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
-
+	assertError := func(t testing.TB, got error, want error) {
+		if got == nil {
+			t.Errorf("wanted an error but didn't get one")
+		}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
@@ -21,6 +27,17 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		// It's important to check that no money was removed from the account
+		// in illegitimate scenarios! In case of withdrawal with insufficient
+		// funds, the final balance should be the same as the starting balance
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
 	})
 
 }
