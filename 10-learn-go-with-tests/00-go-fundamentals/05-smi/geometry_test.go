3df2df41b1b92f8866fb99d467729605e1e0b3c0
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-23 18:54:54 -0400

    Refactor tests for shapes

--- 10-learn-go-with-tests/00-go-fundamentals/05-smi/geometry_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/05-smi/geometry_test.go
@@ -19,23 +19,23 @@ func TestArea(t *testing.T) {
 		t.Helper()
 		got := shape.Area()
 		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+			t.Errorf("%#v got %.2f want %.2f", shape, got, want)
 		}
 	}
 	areaTests := []struct {
-		name  string
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{name: "rectangle 10x10", shape: Rectangle{Length: 10.0, Width: 10.0}, want: 100.0},
-		{name: "rectangle 2x4", shape: Rectangle{Length: 2.0, Width: 4.0}, want: 8.0},
-		{name: "circle r=4", shape: Circle{Radius: 4.0}, want: 16 * math.Pi},
-		{name: "triangle b=2 h=3", shape: Triangle{Base: 2.0, Height: 3.0}, want: 3.0},
+		{name: "rectangle 10x10", shape: Rectangle{Length: 10.0, Width: 10.0}, hasArea: 100.0},
+		{name: "rectangle 2x4", shape: Rectangle{Length: 2.0, Width: 4.0}, hasArea: 8.0},
+		{name: "circle r=4", shape: Circle{Radius: 4.0}, hasArea: 16 * math.Pi},
+		{name: "triangle b=2 h=3", shape: Triangle{Base: 2.0, Height: 3.0}, hasArea: 3.0},
 	}
 	for _, tt := range areaTests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			checkArea(t, tt.shape, tt.want)
+			checkArea(t, tt.shape, tt.hasArea)
 		})
 	}
 }
