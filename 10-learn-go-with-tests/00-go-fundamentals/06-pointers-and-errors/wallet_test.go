92647044766146ecf41a6f253774eab7787372bf
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-23 20:17:46 -0400

    Add assertion when expecting no error

--- 10-learn-go-with-tests/00-go-fundamentals/06-pointers-and-errors/wallet_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/06-pointers-and-errors/wallet_test.go
@@ -3,21 +3,6 @@ package pointersanderrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-	assertError := func(t testing.TB, got error, want error) {
-		if got == nil {
-			t.Errorf("wanted an error but didn't get one")
-		}
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
@@ -25,7 +10,8 @@ func TestWallet(t *testing.T) {
 	})
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
@@ -39,5 +25,25 @@ func TestWallet(t *testing.T) {
 		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingBalance)
 	})
+}
 
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+func assertError(t testing.TB, got error, want error) {
+	if got == nil {
+		t.Errorf("wanted an error but didn't get one")
+	}
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+func assertNoError(t testing.TB, got error) {
+	if got != nil {
+		t.Errorf("got an error but didn't want one")
+	}
 }
