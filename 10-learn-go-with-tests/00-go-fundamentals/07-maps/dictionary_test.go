0facd04550d53c69ccc960c18c3a0a801e5321e6
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-25 14:42:59 -0400

    Deprecate raw map use case

--- 10-learn-go-with-tests/00-go-fundamentals/07-maps/dictionary_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/07-maps/dictionary_test.go
@@ -5,14 +5,6 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	t.Run("raw map", func(t *testing.T) {
-		dictionary := map[string]string{"test": "this is just a test"}
-		searchTerm := "test"
-		got := Search(dictionary, searchTerm)
-		want := "this is just a test"
-		assertString(t, got, want)
-	})
-
 	t.Run("Dictionary wrapper type", func(t *testing.T) {
 		dictionary := Dictionary{"test": "this is just a test"}
 		searchTerm := "test"
