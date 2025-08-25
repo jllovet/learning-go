d01319dc78ea82f6f5a5ebfb03ac913e1de97056
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-25 14:42:35 -0400

    Add custom Dictionary type to wrap map[string]string

--- 10-learn-go-with-tests/00-go-fundamentals/07-maps/dictionary_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/07-maps/dictionary_test.go
@@ -5,11 +5,22 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
-	searchTerm := "test"
-	got := Search(dictionary, searchTerm)
-	want := "this is just a test"
-	assertString(t, got, want)
+	t.Run("raw map", func(t *testing.T) {
+		dictionary := map[string]string{"test": "this is just a test"}
+		searchTerm := "test"
+		got := Search(dictionary, searchTerm)
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+
+	t.Run("Dictionary wrapper type", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+		searchTerm := "test"
+		got := dictionary.Search(searchTerm)
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+
 }
 
 func assertString(t testing.TB, got, want string) {
