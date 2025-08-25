7142cc8a3ff2a9a1c1bdbedd7529a4c1f9c8f6a8
Jonathan Llovet <jllovet@openingquestions.com>
2025-08-25 19:11:58 -0400

    Rewrite tests to check for correct output

--- 10-learn-go-with-tests/00-go-fundamentals/09-mocks/countdown.go
+++ 10-learn-go-with-tests/00-go-fundamentals/09-mocks/countdown.go
@@ -7,7 +7,10 @@ import (
 )
 
 func Countdown(w io.Writer) {
-	fmt.Fprintf(w, "3")
+	fmt.Fprintf(w, "3\n")
+	fmt.Fprintf(w, "2\n")
+	fmt.Fprintf(w, "1\n")
+	fmt.Fprintf(w, "Go!")
 }
 
 func main() {
