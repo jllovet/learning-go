9ab85bf6b621b023bb52f0d5b25a8f14675266f8
Jonathan Llovet <jllovet@openingquestions.com>
2025-09-06 11:48:07 -0400

    Add baseline benchmark

--- 10-learn-go-with-tests/00-go-fundamentals/10-concurrency/concurrency_test.go
+++ 10-learn-go-with-tests/00-go-fundamentals/10-concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package concurrency_test
 import (
 	"reflect"
 	"testing"
+	"time"
 
 	concurrency "github.com/jllovet/learning-go/10-learn-go-with-tests/00-go-fundamentals/10-concurrency"
 )
@@ -26,5 +27,20 @@ func TestCheckWebsites(t *testing.T) {
 	if !reflect.DeepEqual(want, got) {
 		t.Errorf("want %v but got %v", want, got)
 	}
+}
 
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := range len(urls) {
+		urls[i] = "a url"
+	}
+
+	for b.Loop() {
+		concurrency.CheckWebsites(slowStubWebsiteChecker, urls)
+	}
 }
