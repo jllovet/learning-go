1facf1b083000afa74890b2c026f6310eb5856e3
Jonathan Llovet <jllovet@openingquestions.com>
2025-09-06 12:28:48 -0400

    Refactor to use goroutines with channels

--- 10-learn-go-with-tests/00-go-fundamentals/10-concurrency/concurrency.go
+++ 10-learn-go-with-tests/00-go-fundamentals/10-concurrency/concurrency.go
@@ -3,10 +3,22 @@ package concurrency
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	type result struct {
+		url   string
+		valid bool
+	}
+	resultChannel := make(chan result)
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func() {
+			resultChannel <- result{url: url, valid: wc(url)}
+		}()
+	}
+
+	for range len(urls) {
+		r := <-resultChannel
+		results[r.url] = r.valid
 	}
 
 	return results
